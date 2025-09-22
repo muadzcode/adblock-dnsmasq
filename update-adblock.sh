b5815737ff8f35222587b6f13541f25fec219a61
muadztech <muadzjabal1102@gmail.com>
2025-09-22 23:23:39 +0800

    Update adblock script

--- update-adblock.sh
+++ update-adblock.sh
@@ -1,40 +1,23 @@
 #!/bin/bash
-# Auto AdBlock Updater for dnsmasq
-# Covers Spotify + YouTube (extendable)
 
-CONF_FILE="/etc/dnsmasq.d/adblock.conf"
-TMP_FILE="/tmp/adblock.tmp"
+BLOCKLIST="/etc/dnsmasq.d/blocklist.conf"
+TMP_LIST="/tmp/blocklist.raw"
 
-echo "# Master AdBlock Config (Auto Updated: $(date))" > $TMP_FILE
+# Download multiple sources
+curl -s https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts \
+    https://adaway.org/hosts.txt \
+    https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext \
+    https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/SmartTV-AGH.txt \
+    https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/AmazonFireTV.txt \
+    https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/SpotifyAds.txt \
+    https://raw.githubusercontent.com/yous/YoutubeAdsBlocklist/master/domains.txt \
+    | grep '^0.0.0.0' > "$TMP_LIST"
 
-# Spotify custom entries
-cat <<EOL >> $TMP_FILE
-# === Spotify ===
-address=/ads.spotify.com/0.0.0.0
-address=/adclick.g.doubleclick.net/0.0.0.0
-address=/pubads.g.doubleclick.net/0.0.0.0
-address=/partnerad.l.doubleclick.net/0.0.0.0
-EOL
+# Clean duplicates
+sort -u "$TMP_LIST" > "$BLOCKLIST"
 
-# YouTube custom entries
-cat <<EOL >> $TMP_FILE
-# === YouTube ===
-address=/ads.youtube.com/0.0.0.0
-address=/s.youtube.com/0.0.0.0
-address=/s2.youtube.com/0.0.0.0
-address=/pagead2.googlesyndication.com/0.0.0.0
-address=/pubads.g.doubleclick.net/0.0.0.0
-address=/googleads.g.doubleclick.net/0.0.0.0
-EOL
-
-# Add external blocklist (optional: StevenBlack hosts)
-curl -s https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts | \
-grep "^0.0.0.0" | awk '{print "address=/"$2"/0.0.0.0"}' >> $TMP_FILE
-
-# Replace old config
-sudo mv $TMP_FILE $CONF_FILE
+# Convert to dnsmasq format
+awk '/^0.0.0.0/ {print "address=/" $2 "/0.0.0.0"}' "$BLOCKLIST" > /etc/dnsmasq.d/adblock.conf
 
 # Restart dnsmasq
-sudo systemctl restart dnsmasq
-
-echo "AdBlock updated & dnsmasq restarted!"
+systemctl restart dnsmasq
